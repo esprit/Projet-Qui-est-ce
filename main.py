ba90eb30c08aa3d176c5d2d8411958c808320487
MatheoBeu <79721232+MatheoBeu@users.noreply.github.com>
2022-02-07 23:24:10 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from tkinter import *
 import tkinter as tk
+from recup import *
+from PIL import Image, ImageTk
+from tkinter import ttk
 
-
-import recup
-
-recup.test()
 win= Tk()
 win.title("Qui est-ce ?")
-win.geometry("750x550") 
+win.geometry("750x550")
 win.minsize(700,500)
-win.iconphoto(True, tk.PhotoImage("images/logo.jpg"))
+
 win.config(background='grey')
 
 
@@ -17,19 +16,31 @@ label_title = Label(win, text="C KI ?",font=("Courrier",40), bg='grey')
 label_title.pack()
 
 
-def destroy_window():
+def window_game():
   win.destroy()
-  
-def new_window():
+  affichage_possibilite()
   win2= Tk()
   win2.geometry("750x550")
-  win.destroy()
+  taille = len(mes_images)
+  nbrCol = int(nbrColonne())
+  nbrLi = int(nbrLigne())
+  incr = 0
+  for i in range (nbrCol) :
+    for y in range(nbrLi):
+      if taille-1 > i+y :
+        image = ImageTk.PhotoImage(Image.open(mes_images[incr]))
+        panel = Label(win2, image=image)
+        panel.image = image
+        panel.grid(row=y, column=i, sticky='nw')
+        incr += 1
 
-Start_Button = Button(win, text= "Play", font=("Courrier",20), bg='white',command=new_window)
+ 
 
-Leave_Button = Button(win, text= "Leave", font=("Courrier",20),bg='white',command=destroy_window)
-
+Start_Button = Button(win, text= "Play", font=("Courrier",20), bg='white',command=window_game)
 Start_Button.pack(pady=25)
+Leave_Button = Button(win, text= "Leave", font=("Courrier",20),bg='white',command=win.quit)
 Leave_Button.pack(pady=25)
-win.mainloop()
 
+
+
+win.mainloop()
