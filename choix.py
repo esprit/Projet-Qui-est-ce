cf035e8e2f6e77719ab3bb0af6562ee1c6f6e729
Audrey3344 <98320202+Audrey3344@users.noreply.github.com>
2022-02-14 18:38:26 +0100

    Add files via upload
    Création choix multiple

--- choix.py
+++ choix.py
@@ -1,56 +1,87 @@
 from tkinter import *
 import tkinter as tk
 import tkinter.ttk
+from recup import *
 
-
-win_game= Tk()
-win_game.title("Qui est-ce ?2")
-win_game.geometry("750x550")
-win_game.minsize(700,500)
+win_game = Tk()
+win_game.title("Qui est-ce ?")
+win_game.geometry("300x550")
+win_game.minsize(700, 500)
 
 win_game.config(background='grey')
 
-frame=Frame(win_game,bg="pink")
+frame = Frame(win_game,relief=RAISED, bg="pink", bd=4,)
+frame.place(x=400, y=300)
 
-label_title = Label(frame,font=("Courrier",12), bg="pink",text="Choisissez votre question !")
+
+label_title = Label(frame,
+                    font=("Courrier", 12),
+                    bg="pink",
+                    text="  Choisissez votre question !  ")
 label_title.pack(pady=20)
 
-def action(event) : 
-  select = cb.get()
-  if select == "Genre" :
-    cb1.pack(pady=25)
-    cb2.pack_forget()
-    cb3.pack_forget()
-  elif select == "cheveux" :
-    cb3.pack(pady=25)
-    cb1.pack_forget()
-    cb2.pack_forget()
-  elif select == "lunettes" or "chauve" :
-    cb2.pack(pady=25)
-    cb1.pack_forget()
-    cb3.pack_forget()
+
+def action2(event):
+  liste=tri_keys(cb.get())
+  cb1["values"]=liste
+  cb1.set(":)")
 
 
-def combobox_choix():
-  cb = tkinter.ttk.Combobox(frame,state="readonly")
-  cb['values'] = ["Genre","cheveux","lunettes","chauve"]
-  cb.set("choix")
-  cb.bind("<<ComboboxSelected>>", action)
-  cb.pack(pady=25)
+def action3(event):
 
-  cb1 = tkinter.ttk.Combobox(frame,state="readonly")
-  cb1['values']=["Femme","Homme"]
-  cb1.set("Femme ou Homme")
-
-  cb2 = tkinter.ttk.Combobox(frame,state="readonly")
-  cb2['values']=["oui","non"]
-  cb2.set("Oui ou Non")
-
-  cb3 = tkinter.ttk.Combobox(frame,state="readonly")
-  cb3['values']=["Blond","Blanc","Brun","Chatain","Roux"]
-  cb3.set("couleur de cheveux ?")
+  
+  liste=tri_keys(cbPlusChoix.get())
+  
+  cbPlusValeurs["values"]=liste
+  cbPlusValeurs.set(":)")
+ 
 
 
-frame.pack(side=RIGHT, padx=10)
+
+
+def OuvreChoix():
+
+  global cbPlusChoix
+  cbPlusChoix=tkinter.ttk.Combobox(frame, state="readonly",values=l)
+  global cbPlusValeurs
+  cbPlusValeurs=tkinter.ttk.Combobox(frame, state="readonly")
+  cbPlusChoix.set("choix")
+  cbPlusChoix.pack(pady=15)
+  cbPlusValeurs.pack(pady=10)
+  cbPlusChoix.bind("<<ComboboxSelected>>",action3)
+
+l=recup_keys()
+del l[0:2]
+cb = tkinter.ttk.Combobox(frame, state="readonly",values=l)
+cb1 = tkinter.ttk.Combobox(frame,state="readonly")
+
+
+
+
+
+cb.set("choix")
+cb.bind("<<ComboboxSelected>>", action2)
+cb.pack(pady=15)
+cb1.pack(pady=15)
+
+
+
+def ET():
+  OuvreChoix()
+  et = Button(frame,text="ET",command=ET)
+  et.pack(pady=10,padx=30)
+
+def OU():
+  OuvreChoix()
+  ou = Button(frame,text="OU",command=OU)
+  ou.pack(pady=10,padx=30)
+
+et = Button(frame,text="ET",command=ET)
+et.pack(pady=10,padx=30)
+
+ou = Button(frame,text="OU",command=OU)
+ou.pack(pady=10,padx=30)
+
+frame.pack(side=TOP, padx=10)
 
 win_game.mainloop()
