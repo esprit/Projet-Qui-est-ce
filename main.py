b39fcbd69d7025b2dc4e21d852258e45ef29578f
MatheoBeu <79721232+MatheoBeu@users.noreply.github.com>
2022-02-19 20:23:07 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -16,24 +16,45 @@ label_title = Label(win, text="C KI ?",font=("Courrier",40), bg='grey')
 label_title.pack()
 
 
+
 def window_game():
   win.destroy()
   affichage_possibilite()
   win2= Tk()
   win2.geometry("750x550")
+
+  wrapper1 = LabelFrame(win2)
+  
+  mycanvas = Canvas(wrapper1)
+  mycanvas.pack(side=LEFT, fill="both", expand="yes")
+
+  yscrollbar = ttk.Scrollbar(wrapper1, orient="vertical",command=mycanvas.yview)
+  yscrollbar.pack(side=RIGHT, fill="y")
+
+  mycanvas.configure(yscrollcommand=yscrollbar.set)
+
+  mycanvas.bind('<Configure>',  lambda e: mycanvas.configure(scrollregion = mycanvas.bbox('all')))
+
+  
+  myframe = Frame(mycanvas)
+  mycanvas.create_window((0,0), window=myframe, anchor="nw")
+  
   taille = len(mes_images)
   nbrCol = int(nbrColonne())
   nbrLi = int(nbrLigne())
   incr = 0
+
+
   for i in range (nbrCol) :
     for y in range(nbrLi):
       if taille-1 > i+y :
         image = ImageTk.PhotoImage(Image.open(mes_images[incr]))
-        panel = Label(win2, image=image)
+        panel = Label(myframe, image=image)
         panel.image = image
         panel.grid(row=y, column=i, sticky='nw')
         incr += 1
-
+        
+  wrapper1.pack(fill="both", expand="yes", padx=10, pady=10)
  
 
 Start_Button = Button(win, text= "Play", font=("Courrier",20), bg='white',command=window_game)
@@ -44,3 +65,4 @@ Leave_Button.pack(pady=25)
 
 
 win.mainloop()
+
