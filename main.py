ce810e3076ba447bd86a2b348e8742a76269dc52
icarus213 <79603405+icarus213@users.noreply.github.com>
2022-02-24 16:19:18 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -52,10 +52,41 @@ def window_game():
         panel = Label(myframe, image=image)
         panel.image = image
         panel.grid(row=y, column=i, sticky='nw')
+        panel.bind('<Button-1>', lambda event, r=y,c=i,h=image.height(),w=image.width(),nom_img=mes_images[incr],id=incr:on_click(event,r,c,h,w,nom_img,id))
+        
         incr += 1
         
   wrapper1.pack(fill="both", expand="yes", padx=10, pady=10)
- 
+
+  #fonction on_click sur une image sans croix
+  def on_click(event,r,c,h,w,nom_img,id):
+    global image
+    red = Image.open("images/"+"red.png")   
+    red2= red.resize((w,h), Image.ANTIALIAS)
+    image = Image.open(nom_img)
+  
+    image.paste(red2,(0,0),red2)
+    image.save('red2.png',"PNG")
+    
+    photo2 = ImageTk.PhotoImage((Image.open("red2.png")).resize((w,h), Image.ANTIALIAS))
+    
+    red = tk.Label(myframe, image=photo2)
+    
+    red.image = photo2
+    red.grid(row=r,column=c, sticky='nw')
+    print("r1=",r,"  c1=",c)
+    red.bind('<Button-1>', lambda event, r=r,c=c,h=h,w=w,nom_img=nom_img,id=id:on_reclick(event,r,c,h,w,nom_img,id))
+    
+    
+    
+  #fonction on_click sur une image avec croix
+  def on_reclick(event,r,c,h,w,nom_img,id):
+    image = ImageTk.PhotoImage(Image.open(nom_img))
+    panel = Label(myframe, image=image)
+    panel.image = image
+    panel.grid(row=r, column=c, sticky='nw')
+    panel.bind('<Button-1>', lambda event, r=r,c=c,h=h,w=w,nom_img=nom_img,id=id:on_click(event,r,c,h,w,nom_img,id))
+    
 
 Start_Button = Button(win, text= "Play", font=("Courrier",20), bg='white',command=window_game)
 Start_Button.pack(pady=25)
@@ -65,4 +96,3 @@ Leave_Button.pack(pady=25)
 
 
 win.mainloop()
-
