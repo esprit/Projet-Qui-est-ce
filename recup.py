cda2b2d73673dda4a28da1d808036ddd96020178
MatheoBeu <79721232+MatheoBeu@users.noreply.github.com>
2022-03-05 20:15:36 +0100

    Add files via upload

--- recup.py
+++ recup.py
@@ -1,38 +1,48 @@
 from tkinter import *
 import tkinter as tk
 import json
+import os
+from PIL import ImageTk
 import random
 
 mes_images = []
 nbrCol = 0
 nbrLi = 0
 
-def affichage_possibilite():    
+def localisation_json(loca_json):
+    global fichier
+    fichier = loca_json
+
+def liste_json():
+    liste = os.listdir("json")
+    return liste
+
+def affichage_possibilite():
     mes_perso = []
-    with open('perso.json', 'r') as f:
+    with open("json/" + fichier, 'r',encoding='utf-8') as f:
         data = json.load(f)
     possibilites = data["possibilites"]
     for i in data["possibilites"]:
         perso = possibilites[str(i)]
-        my_img = "personnages/" + perso["fichier"]
+        my_img = "images_theme/" + data["images"] + perso["fichier"]
         mes_images.append(my_img)
         mes_perso.append(perso["nom"])
-
-    
-def recup_keys(): #retourne la liste des keys qui décrivent les perso: exemple: ['fichier', 'prenom', 'genre', 'cheveux', 'lunettes', 'chauve']
-  with open('perso.json', 'r') as f:
+def recup_keys(): #retourne la liste des keys qui décrivent les perso: exemple: ['fichier', 'nom', 'genre', 'cheveux', 'lunettes', 'chauve']
+  with open("json/" + fichier, 'r',encoding='utf-8') as f:
     data = json.load(f)
     possibilites = data["possibilites"]
+
     infoPerso = possibilites["0"]
     keys = list(infoPerso)  #liste des keys du dictionnaire
+
     # for i in range(len(keys)) :  #affiche toutes les key du dictionnaire
     #   print(keys[i])
-    return keys 
 
-    
-#fonction qui ranvoie le perso choisi par l'ordi  
+    return keys
+
+#fonction qui ranvoie le perso choisi par l'ordi
 def randompersonnage ():
-  with open('perso.json', 'r') as f:
+  with open("json/" + fichier, 'r',encoding='utf-8') as f:
     data = json.load(f)
     possibilites = data["possibilites"]
     nbperso=int(data["colonne"])*int(data["ligne"])
@@ -43,18 +53,17 @@ def randompersonnage ():
 
 
 def nbrColonne():
-  with open('perso.json', 'r') as f:
-        data = json.load(f) 
+  with open("json/" + fichier, 'r',encoding='utf-8') as f:
+        data = json.load(f)
   return data["colonne"]
 
-def nbrLigne(): 
-  with open('perso.json', 'r') as f:
+def nbrLigne():
+  with open("json/" + fichier, 'r',encoding='utf-8') as f:
         data = json.load(f)
   return data["ligne"]
 
-# crée une liste de valeur pour chaque key
-def tri_keys(caract): 
-  with open('perso.json', 'r') as f:
+def tri_keys(caract):
+  with open("json/" + fichier, 'r',encoding='utf-8') as f:
     data = json.load(f)
     possibilites = data["possibilites"]
     nbperso=int(data["colonne"])*int(data["ligne"])
@@ -65,8 +74,8 @@ def tri_keys(caract):
   return(possibles)
 
 #crée une liste de prénom correspondant à la valeur du caractere (toute les personnes qui ont les cheveux roux par ex)
-def creerliste(caract,valeur): 
-  with open('perso.json', 'r') as f:
+def creerliste(caract,valeur):
+  with open("json/" + fichier, 'r',encoding='utf-8') as f:
     data = json.load(f)
     possibilites = data["possibilites"]
     nbperso=int(data["colonne"])*int(data["ligne"])
